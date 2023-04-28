3ceb6e27b976bea092f876666f219505f97c1134
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-04-28 18:53:04 -0300

    Adicionando projeto inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val titular: String = "Walisson"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += + 200
+    
+    println("$titular")
+    println("$numeroConta")
+    println("$saldo")
+}
+
+
