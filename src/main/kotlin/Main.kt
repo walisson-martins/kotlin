b1d609a84c9c5a314d4a224a68fbadfa514d32ef
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-04-28 19:37:27 -0300

    paradigma funcional com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,46 @@
 fun main() {
-    val titular: String = "Walisson"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += + 200
-    
-    println("$titular")
-    println("$numeroConta")
-    println("$saldo")
+
+    var i = 0
+    while(i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 0.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+//    for (i in 5 downTo 1) {
+//        println("Bem vindo ao Bytebank")
+//
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = i + 0.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//
+//        when {
+//            saldo > 0.0 -> println("Conta é positiva")
+//            saldo == 0.0 -> println("Conta é positiva")
+//            else -> println("Conta é negativa")
+//        }
+//    }
+
+//    testaCondicoes(saldo)
 }
 
-
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra")
+    } else {
+        println("Conta é negativa")
+    }
+}
