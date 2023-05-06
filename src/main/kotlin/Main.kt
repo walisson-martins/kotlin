fd63962fa2d31b3a7cdbe749af230fb2cf1f1ec2
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-06 15:58:41 -0300

    Condicionais com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,73 @@
 import kotlin.math.max
 
 fun main() {
-    helloWorld()
-    sum(2, 3)
-    println("${divisao(10f, 3.5f)}")
+    convertYears(2)
+    caracters("calculo de string")
+    cubo(3)
+    converteMilhas(1.00f)
+    trocaLetras("asdxa asd cx")
+    maiorDeIdade(15)
 
-    val str = "Tamanho da String"
-
-    println(max(5, 10))
+    val bonus = bonus("Gerente")
+    println(bonus)
 }
 
-fun sumRedux(a: Int, b: Int) = (a + b)
+fun bonus(cargo: String): Float {
+    var bonus = 0f
+    if (cargo == "Gerente") {
+        bonus = 2000f
+    } else if (cargo == "Coordenador") {
+        bonus = 1500f
+    } else if (cargo == "Engenheiro de software") {
+        bonus = 1000f
+    } else if (cargo == "Estagiário") {
+        bonus = 500f
+    }
 
-fun sum(a: Int, b: Int): Int {
-    return (a + b)
+    return bonus
 }
 
-fun helloWorld(): Unit {
-    println("Hello World")
+fun maiorDeIdade(idade: Int) {
+    if (idade > 18) {
+        println("Maior de idade")
+    } else if (idade < 10) {
+        println("Menor de idade")
+    }
 }
 
-fun divisao(a: Float, b: Float) = a / b
+fun saudacao(dia: Boolean) = if (dia) {
+    "Bom dia"
+} else {
+    "Boa noite"
+}
 
+fun retorno(valor: Int): Int {
+    return if (valor != 10) {
+        10
+    } else {
+        20
+    }
+}
+
+fun trocaLetras(str: String): String {
+    return str.replace("A", "x").lowercase()
+}
+
+fun converteMilhas(milhas: Float) = milhas * 1.6F
+
+fun cubo(n: Int): Int {
+    return n * n * n
+}
+
+fun caracters(str: String): Int {
+    return str.length
+}
+
+fun convertYears(year: Int) {
+    println("$year anos equivale a:")
+    println("${year * 12} meses:")
+    println("${year * 365} dias:")
+    println("${year * 365 * 24} horas:")
+    println("${year * 365 * 24 * 60} minutos:")
+    println("${year * 365 * 24 * 60} segundos:")
+}
