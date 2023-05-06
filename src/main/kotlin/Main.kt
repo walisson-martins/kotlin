6151461ea6b6ccd40b4a4da53c903035f149bdc8
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-06 17:22:36 -0300

    Exception, let, elvis operator com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,85 +1,25 @@
+import java.lang.Exception
+
 fun main() {
-    portaria()
-}
+    try {
+        val str: String? = null
+        println(str ?: "nulo")
 
-fun portaria() {
-    println("Qual sua idade")
-    val idade = readLine()
+        var str2 = if (str == null) "maior de idade" else "menor de idade"
 
-    if (idade != null && idade != "") {
-        if (idade.toInt() < 18) {
-            println("Negado")
-            return
+        str?.let {
+            it.lowercase()
+            it.length
         }
-    }
 
-    println("Qual o tipo de convite")
-    var tipoConvite = readLine()
 
-    if (tipoConvite != null && tipoConvite != "") {
-        tipoConvite = tipoConvite.lowercase()
-
-        if (tipoConvite == "comum" || tipoConvite == "premium" || tipoConvite == "luxo") {
-
-            println("Qual o código do convite?")
-            var codigo = readLine()
-
-            if (codigo != null && codigo != "") {
-                codigo = codigo.lowercase()
-                if (tipoConvite == "comum" && codigo.startsWith("xt")) {
-                    println("Bem vindo")
-                } else if (tipoConvite == "premium" || tipoConvite == "luxo" && codigo.startsWith("xl")) {
-                    println("Bem vindo")
-                } else {
-                    println("Negado: convite não encontrado")
-                }
-            }
-        } else {
-            println("Negado: convite inválido")
-            return
-        }
-    }
-
-}
-
-fun square() {
-    println("informe o lado 1:")
-    val lado1 = readLine()
-
-    println("informe o lado 2:")
-    val lado2 = readLine()
-
-    if (lado1 != null && lado1 != "" && lado2 != null && lado2 != "") {
-        val x = lado1.toInt()
-        val y = lado2.toInt()
-
-        if (x == y) {
-            println("Quadrado")
-        } else {
-            println("Não é um quadrado")
-        }
-    }
-}
-
-fun triangulo() {
-    println("informe o lado 1:")
-    val lado1 = readLine()
-
-    println("informe o lado 2:")
-    val lado2 = readLine()
-
-    println("informe o lado 3:")
-    val lado3 = readLine()
-
-    if (lado1 != null && lado1 != "" && lado2 != null && lado2 != "" && lado3 != null && lado3 != "") {
-        val x = lado1.toInt()
-        val y = lado2.toInt()
-        val z = lado3.toInt()
-
-        if (x == y && y == z) {
-            println("Triângulo")
-        } else {
-            println("Não é um Triângulo")
-        }
+    } catch (e: NullPointerException) {
+        println("Variável inválida")
+    } catch (e: ArithmeticException) {
+        println("ArithmeticException inválida")
+    } catch (e: Exception) {
+        println("Exception")
+    } finally {
+        println("Finally")
     }
 }
