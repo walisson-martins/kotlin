c9d09d7e2d52cc49e30656513ef2ec856d60276a
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-09 11:33:52 -0300

    Genericas e vararg com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,20 @@
-fun portaria(idade: Int, tpConvite: String, cod: String): String {
-    if (idade < 18) {
-        return "Negado."
-    }
+fun main() {
+    endereco("street", "city", "DF", "73358315")
+    endereco(cidade = "ci", rua = "street", estado = "Goias")
+    media(1f, 2f, 3f)
+}
 
-    if (tpConvite != "") {
-        val tipoConvite = tpConvite.lowercase()
-
-        if (tipoConvite != "comum" && tipoConvite != "premium" && tipoConvite != "luxo") {
-            return "Negado."
-        }
-
-        if (cod != "") {
-            val codigo = cod.lowercase()
-            return if (tipoConvite == "comum" && codigo.startsWith("xt")) {
-                "Welcome."
-            } else if ((tipoConvite == "premium" || tipoConvite == "luxo") && codigo.startsWith("xl")) {
-                "Welcome."
-            } else {
-                "Negado."
-            }
+//vararg - argumentos de parametros
+fun <T> media(vararg notas: T): Float {
+    var soma = 0f
+    for (n in notas) {
+        if (n is Float) {
+            soma += n
         }
     }
-    return "Negado."
+
+    return (soma / notas.size)
+}
+
+fun endereco(rua: String, cidade: String, estado: String, cep: String = "", num: Int = 0) {
 }
