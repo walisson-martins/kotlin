dc6aa562007269112f2cf783384bc363235b42cb
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-09 12:37:52 -0300

    With e lateinit com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,14 @@
-enum class Prioridade(val value: Int) {
-    Baixa(1) {
-        override fun toString(): String {
-            return super.toString()
-        }
-    },
-    Media(5),
-    Alta(10)
-}
-
-enum class AnimalEnum {
-    Cachorro, Gato, Cavalo, Vaca
-}
-
-fun chamada(x: Prioridade) {}
 fun main() {
-    chamada(Prioridade.Media)
-    for (p in Prioridade.values()) {
-        println(p)
-    }
+    val p = Pessoa(anoNascimento, nome)
 
-    var f1: Forma = Forma(10, 8)
-    var f2: Forma = Forma(10, 8)
-
-    var f3: FormaData = FormaData(10, 8)
-    var f4: FormaData = FormaData(10, 8)
-
-    f1.equals(f2)
-    //f1.equals()
-    println(f1.toString())
-    println(f1.hashCode())
-    println(f2.toString())
-    println(f2.hashCode())
-
-    f4 = f3.copy()
-}
-
-class Forma(val a: Int, val b: Int) {
-    override fun equals(other: Any?): Boolean {
-        return if (other is Forma) {
-            (other.a == this.a && other.b == this.b)
-        } else {
-            return false
-        }
+    with(p) {
+        acordar()
+        dormir()
     }
 }
 
-data class FormaData(val a: Int, val b: Int) {
-
+class Receita {
+  lateinit var instrucoes: String
+  fun geraReceita() {
+  }
 }
