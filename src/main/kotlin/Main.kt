ada03a68d5d9d95448de5235f0302471dc5f2984
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-09 12:30:56 -0300

    Data Class e Class com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,50 @@
-class Animal(var especie: String) {
-    var fala: String = ""
-        get() {
-            println("Acesso get")
-            return field
+enum class Prioridade(val value: Int) {
+    Baixa(1) {
+        override fun toString(): String {
+            return super.toString()
         }
-        set(value) {
-            println("Acesso set")
-            fala = value
-        }
-
-    init {
-        fala = if (especie == "cachorro") {
-            "au"
-        } else if (especie == "gato") {
-            "miau"
-        } else {
-            ""
-        }
-    }
-
-    fun falar() {
-        println(fala)
-    }
-
-    init {
-        println("2 Animal")
-    }
+    },
+    Media(5),
+    Alta(10)
 }
 
+enum class AnimalEnum {
+    Cachorro, Gato, Cavalo, Vaca
+}
+
+fun chamada(x: Prioridade) {}
 fun main() {
-    val animal = Animal("cachorro")
-    Animal("gato").falar()
-    animal.falar()
+    chamada(Prioridade.Media)
+    for (p in Prioridade.values()) {
+        println(p)
+    }
+
+    var f1: Forma = Forma(10, 8)
+    var f2: Forma = Forma(10, 8)
+
+    var f3: FormaData = FormaData(10, 8)
+    var f4: FormaData = FormaData(10, 8)
+
+    f1.equals(f2)
+    //f1.equals()
+    println(f1.toString())
+    println(f1.hashCode())
+    println(f2.toString())
+    println(f2.hashCode())
+
+    f4 = f3.copy()
 }
 
+class Forma(val a: Int, val b: Int) {
+    override fun equals(other: Any?): Boolean {
+        return if (other is Forma) {
+            (other.a == this.a && other.b == this.b)
+        } else {
+            return false
+        }
+    }
+}
+
+data class FormaData(val a: Int, val b: Int) {
+
+}
