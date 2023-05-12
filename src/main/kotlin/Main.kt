2078790c0b8043c0d4a26228b5fea20d72910264
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-12 19:53:33 -0300

    Interface e Abstract com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,27 @@
-class Matematica {
+interface Selvagem {
+    fun atacar() {
+        println("")
+    }
+}
 
-    companion object {
-        val PI = 3.1415
-        fun funcao() {}
+abstract class Mamifero(val nome: String) {
+    fun acordar() {}
+    fun dormir() {}
+    abstract fun falar()
+    abstract fun comer()
+}
+
+
+class Cachorro(nome: String) : Mamifero(nome), Selvagem {
+    override fun falar() {
+        TODO("Not yet implemented")
     }
 
-    object obj {
-        val PI = 3.1415
-        fun funcao() {}
+    override fun comer() {
+        TODO("Not yet implemented")
     }
 }
 
 fun main() {
-    Matematica.PI
-    Matematica.funcao()
-    Matematica.obj.PI
+
 }
