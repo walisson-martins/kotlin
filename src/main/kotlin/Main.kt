7df6eeee1ed3d1c1f2d824ae43b4725c382afc2b
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-12 19:29:27 -0300

    Polimorfismo e Override com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,29 @@
+open class Eletronico(var marca: String) {
+    private fun corrente(ativo: Boolean) {}
+    fun ligar() {
+        corrente(false)
+    }
+
+    open fun desligar() {}
+}
+
+class Computador(marca: String) : Eletronico(marca) {
+    fun save() {}
+
+    override fun desligar() {
+        save()
+        super.desligar()
+    }
+
+    fun save(a: Int) {
+    }
+
+    fun save(a: Double) {
+    }
+}
+
 fun main() {
     var c: Computador = Computador("DELL")
     c.ligar()
     c.desligar()
-}
-
-open class Eletronico(private var marca: String) {
-    private fun ativarCorrente() {}
-    fun ligar() {
-        ativarCorrente()
-    }
-
-   fun desligar() {}
-}
-
-class Computador(marca: String) : Eletronico(marca) {
-    fun software() {}
-    fun processar() {}
 }
