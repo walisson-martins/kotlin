00d950678c6d732d5afcd994935906aed60ac00e
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-26 10:41:09 -0300

    Funções de Coletions com Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,64 @@
 fun main() {
-    val set1: Set<String> = setOf<String>("paris", "berlim", "paris")
-    val set2: MutableSet<String> = mutableSetOf<String>()
+    val data = listaDeDados()
+    data.any()
+    data.count()
 
-    println(set1)
+    println("Primeira receita ${data.first()}")
+    println("Ultima receita ${data.last()}")
 
-    val map1 = mapOf<String, String>(Pair("França", "Paris"), Pair("Alemanha", "Berlim"))
-    val map2 = mutableMapOf<String, String>(Pair("França", "Paris"), Pair("Alemanha", "Berlim"))
+    listOf<Int>(1, 2, 3).sum()
+    println(data.sumOf { it.calorias })
 
-    println(map1.entries)
-    println(map1.values)
-    map2["Brasil"] = "Brasilia"
+    data.filter { it.nome == "Lasanha" }
+
+    //pega as duas primeiras e duas últimas
+    data.take(2)
+    data.takeLast(2)
+
+    data.filter { it.calorias > 100 }.forEach { println(it.nome) }
+    data.forEach { println(it.nome) }
+
+    data.maxOf { it.calorias }
+    data.minOf { it.calorias }
+
+    listOf<Int>(1, 3, 4).maxOf { it }
+    listOf<Int>(1, 3, 4).maxOfOrNull { it }
+
+    //Retorna o objeto
+    println(data.maxByOrNull { it.calorias })
+
+    data.map { it.nome }
 }
+
+private fun listaDeDados(): List<Receita> {
+    return listOf(
+        Receita(
+            "Lasanha", 1200,
+            listOf(
+                Ingrediente("Presunto", 5),
+                Ingrediente("Queijo", 10),
+                Ingrediente("Molho de tomate", 2),
+                Ingrediente("Manjerição", 3)
+            )
+        ),
+        Receita("Panqueca", 500),
+        Receita("Omelete", 200),
+        Receita("Parmegiana", 700),
+        Receita("Sopa de feijão", 300),
+        Receita(
+            "Hamburguer", 2000,
+            listOf(
+                Ingrediente("Pão", 1),
+                Ingrediente("Hamburguer", 3),
+                Ingrediente("Queijo", 1),
+                Ingrediente("Catupiry", 1),
+                Ingrediente("Bacon", 3),
+                Ingrediente("Alface", 1),
+                Ingrediente("Tomate", 1)
+            )
+        )
+    )
+}
+
+data class Receita(val nome: String, val calorias: Int, val ingredientes: List<Ingrediente> = listOf())
+data class Ingrediente(val nome: String, val quantidade: Int)
